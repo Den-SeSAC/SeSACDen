f58075cda7c65e311d830103c2347b30ebce7c5c
Den <den@memolease.com>
2024-10-08 14:16:45 +0900

    init

---
+++ SeSACDen/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SeSACDen
+//
+//  Created by Den on 10/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
